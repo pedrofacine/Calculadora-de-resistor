4b13b450370d77687af24231705f97216b7b1195
Pedro Facine Nery <penery06@gmail.com>
2025-02-17 15:48:49 -0300

    Função Obter Multiplicador

--- main.kt
+++ main.kt
@@ -1,4 +1,4 @@
-fun obterValor (cor: string): Int {
+fun obterValor (cor: String): Int {
     return when (cor.lowercase()) {
         "preto" -> 0
         "marrom" -> 1
@@ -11,5 +11,22 @@ fun obterValor (cor: string): Int {
         "cinza" -> 8
         "branco" -> 9
         else -> -1
+    }
+}
+fun obterMultip (cor: String): Double{
+    return when (cor.lowercase()){
+        "preto" -> 1.0
+        "marrom" -> 10.0
+        "vermelho" -> 100.0
+        "laranja" -> 1_000.0
+        "amarelo" -> 10_000.0
+        "verde" -> 100_000.0
+        "azul" -> 1_000_000.0
+        "violeta" -> 10_000_000.0
+        "cinza" -> 100_000_000.0
+        "branco" -> 1_000_000_000.0
+        "dourado" -> 0.1
+        "prateado" -> 0.01
+        else -> -1.0
     }
 }
